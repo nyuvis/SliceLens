bae08fd1fd21c36a807282796a225dc6ed8fb808
Daniel Kerrigan <dkerriga@nd.edu>
2020-06-25 15:27:02 -0400

    formatting, highlighting on hover

--- src/components/controls/FeatureEditor.svelte
+++ src/components/controls/FeatureEditor.svelte
@@ -4,13 +4,14 @@ https://www.w3schools.com/howto/howto_css_modals.asp
 -->
 
 <script>
-  import { dataset, metadata } from '../../stores.js';
-  import { onMount, createEventDispatcher } from 'svelte';
+  import { dataset, metadata } from "../../stores.js";
+  import { onMount, createEventDispatcher } from "svelte";
+  import { flip } from "svelte/animate";
 
   import * as d3_array from "d3-array";
   import * as d3_all from "d3";
 
-  const d3 = {...d3_all, ...d3_array,};
+  const d3 = { ...d3_all, ...d3_array };
 
   const dispatch = createEventDispatcher();
 
@@ -30,16 +31,17 @@ https://www.w3schools.com/howto/howto_css_modals.asp
   }));
 
   function onWindowClose() {
-    feature.valueToGroup = new Map(Array.from(
-      groupToValues,
-      ([group, values]) => [...values].map(v => [v, group])
-    ).flat());
+    feature.valueToGroup = new Map(
+      Array.from(groupToValues, ([group, values]) =>
+        [...values].map(v => [v, group])
+      ).flat()
+    );
 
     feature.values = groups.map(d => d.name);
 
     metadata.set($metadata);
 
-    dispatch('close');
+    dispatch("close");
   }
 
   let editingGroupName = null;
@@ -50,9 +52,11 @@ https://www.w3schools.com/howto/howto_css_modals.asp
   let groupBeneath = null;
   let valueDragInProgress = false;
   let groupDragInProgress = false;
+  let groupHandleHover = null;
 
   $: if (!groupDragInProgress) {
     groupBeingDragged = null;
+    groupHandleHover = null;
   }
 
   function dropHandler(event, endGroupIndex) {
@@ -117,8 +121,17 @@ https://www.w3schools.com/howto/howto_css_modals.asp
     groupBeneath = null;
   }
 
-  function getBorderClass(currentIndex, overIndex, startIndex, groupDragInProgress) {
-    if (!groupDragInProgress || startIndex === null || currentIndex !== overIndex) {
+  function getBorderClass(
+    currentIndex,
+    overIndex,
+    startIndex,
+    groupDragInProgress
+  ) {
+    if (
+      !groupDragInProgress ||
+      startIndex === null ||
+      currentIndex !== overIndex
+    ) {
       return "";
     } else if (overIndex <= startIndex) {
       return "border-top";
@@ -126,75 +139,103 @@ https://www.w3schools.com/howto/howto_css_modals.asp
       return "border-bottom";
     }
   }
-
 </script>
 
 <div class="modal-background">
-  <div class="modal-content"
-    on:dragenter={onDragEnterBackground}
-  >
+  <div class="modal-content" on:dragenter={onDragEnterBackground}>
     <div class="header">
       <div class="feature-name large bold">{featureName}</div>
 
-      <div class="gap"></div>
+      <div class="gap" />
 
       <!-- x icon -->
-      <svg xmlns="http://www.w3.org/2000/svg"
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
         class="icon icon-tabler icon-tabler-x"
-        width="24" height="24" viewBox="0 0 24 24"
-        stroke-width="2" stroke="currentColor" fill="none"
-        stroke-linecap="round" stroke-linejoin="round"
-        on:click={onWindowClose}
-      >
-        <path stroke="none" d="M0 0h24v24H0z"/>
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        on:click={onWindowClose}>
+        <path stroke="none" d="M0 0h24v24H0z" />
         <line x1="18" y1="6" x2="6" y2="18" />
         <line x1="6" y1="6" x2="18" y2="18" />
       </svg>
     </div>
 
-    {#if feature.type === "C"}
+    {#if feature.type === 'C'}
       <div class="groups">
-        {#each groups as {name, values}, i}
+        {#each groups as { name, values }, i (name)}
           <div
             class="group-container {getBorderClass(i, groupBeneath, groupBeingDragged, groupDragInProgress)}"
             ondragover="return false"
             on:drop|preventDefault={e => dropHandler(e, i)}
             on:dragenter|stopPropagation={() => onDragEnter(i)}
-          >
-            <div class="group"
-              draggable="true"
+            animate:flip={{ duration: 300 }}>
+            <div
+              class="group"
+              draggable="{groupHandleHover === i}"
               on:dragstart={() => onGroupDragStart(i)}
               on:dragend={onGroupDragEnd}
-              class:highlight={groupBeneath === i && valueDragInProgress}
-            >
-              <div class="group-name-row"
-              >
-                {#if editingGroupName !== i}
-                  <div class="bold group-name">{name}</div>
-                  <div class="gap"></div>
-                  <div class="link edit-name"
-                    on:click={() => editingGroupName = i}
+              class:highlightGroup={(groupBeneath === i && valueDragInProgress) || groupHandleHover === i}>
+              <div class="handle">
+                <!-- vertical grip icon -->
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-grip-vertical"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  on:mouseenter={() => groupHandleHover = i}
+                  on:mouseleave={() => groupHandleHover = null}
                   >
-                    Edit name
-                  </div>
-                {:else}
-                  <input class="bold group-name-input" bind:value={name}>
-                  <div class="link edit-name"
-                    on:click={() => editingGroupName = null}
-                  >
-                    Save name
-                  </div>
-                {/if}
+                  <path stroke="none" d="M0 0h24v24H0z" />
+                  <circle cx="9" cy="5" r="1" />
+                  <circle cx="9" cy="12" r="1" />
+                  <circle cx="9" cy="19" r="1" />
+                  <circle cx="15" cy="5" r="1" />
+                  <circle cx="15" cy="12" r="1" />
+                  <circle cx="15" cy="19" r="1" />
+                </svg>
               </div>
-              {#each [...values] as value}
-                <div class="value small"
-                  draggable="true"
-                  on:dragstart|stopPropagation={() => onValueDragStart(i, value)}
-                  on:dragend={onValueDragEnd}
-                >
-                  {value}
+              <div class="content">
+                <div class="group-name-row">
+                  {#if editingGroupName !== i}
+                    <div class="bold group-name">{name}</div>
+                    <div class="gap" />
+                    <div
+                      class="link edit-name"
+                      on:click={() => (editingGroupName = i)}>
+                      Edit name
+                    </div>
+                  {:else}
+                    <input class="bold group-name-input" bind:value={name} />
+                    <div
+                      class="link edit-name"
+                      on:click={() => (editingGroupName = null)}>
+                      Save name
+                    </div>
+                  {/if}
                 </div>
-              {/each}
+                {#each [...values] as value}
+                  <div
+                    class="value small"
+                    draggable="true"
+                    on:dragstart|stopPropagation={() => onValueDragStart(i, value)}
+                    on:dragend={onValueDragEnd}>
+                    {value}
+                  </div>
+                {/each}
+              </div>
             </div>
           </div>
         {/each}
@@ -204,7 +245,6 @@ https://www.w3schools.com/howto/howto_css_modals.asp
 </div>
 
 <style>
-
   .modal-background {
     position: fixed;
     z-index: 1;
@@ -243,6 +283,7 @@ https://www.w3schools.com/howto/howto_css_modals.asp
     height: 3px;
     background-color: black;
   }
+
   .group-name {
     margin-right: 1em;
   }
@@ -254,38 +295,66 @@ https://www.w3schools.com/howto/howto_css_modals.asp
     margin-right: 1em;
     border: 0;
 
-    font-family: 'Fira Sans', sans-serif;
+    font-family: "Fira Sans", sans-serif;
     font-size: 1rem;
   }
 
   .group-container {
+    border: 1px solid white;
     padding: 0.25em 0;
   }
 
-  .border-bottom {
-    border-bottom: 1px solid black;
-  }
-
-  .border-top {
-    border-top: 1px solid black;
-  }
-
   /* the border stays gray without !important */
-  .highlight {
+  .highlightGroup {
     border: 1px solid black !important;
   }
 
+  .border-bottom {
+    border-bottom: 1px solid black !important;
+  }
+
+  .border-top {
+    border-top: 1px solid black !important;
+  }
+
   .group {
-    padding: 0.5em;
+    padding: 0.5em 0.5em 0.5em 0;
 
     border-radius: 5px;
-    border: 1px solid #E5E5E5;
-    background-color: #E5E5E5;
+    border: 1px solid #e5e5e5;
+    background-color: #e5e5e5;
+
+    display: flex;
   }
 
   .groups {
     flex: 1;
     overflow-y: auto;
+  }
+
+  .group-name-row {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .edit-name, .icon-tabler-grip-vertical {
+    visibility: hidden;
+  }
+
+  .group:hover .edit-name,
+  .group:hover .icon-tabler-grip-vertical {
+    visibility: visible;
+  }
+
+  .handle {
+    display: flex;
+    align-items: center;
+    cursor: move;
+  }
+
+  .content {
+    flex: 1;
   }
 
   .value {
@@ -302,19 +371,6 @@ https://www.w3schools.com/howto/howto_css_modals.asp
     border: 1px solid black;
   }
 
-  .group-name-row {
-    display: flex;
-    flex-direction: row;
-  }
-
-  .edit-name {
-    visibility: hidden;
-  }
-
-  .group:hover .edit-name {
-    visibility: visible;
-  }
-
   .icon-tabler-x {
     cursor: pointer;
     margin-left: 1em;
@@ -323,5 +379,4 @@ https://www.w3schools.com/howto/howto_css_modals.asp
   .icon-tabler-x:hover {
     color: red;
   }
-
 </style>
