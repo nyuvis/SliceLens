c70cca73cb8aaf26ad5d38521d3e0ecab0ca43bc
Daniel Kerrigan <dkerriga@nd.edu>
2021-05-08 15:58:04 -0400

    revamp color legend

--- src/components/visualization/matrix/ColorLegend.svelte
+++ src/components/visualization/matrix/ColorLegend.svelte
@@ -3,29 +3,49 @@
   export let color;
 </script>
 
-<div id="legend">
-  {#each color.domain() as label}
-    <div class="legend-cell">
-      <div class="legend-square" style="background: {color(label)}"></div>
-      <p class="small legend-label">{label}
-      </p>
-    </div>
-  {/each}
-  {#if showPredictions}
-    <div class="legend-cell">
-        <div class="legend-square" id="incorrect"></div>
-        <p class="small legend-label">Incorrect</p>
-    </div>
-  {/if}
+<div id="legend" class="small">
+  <div class="bold title">{showPredictions ? 'Predicted Labels' : 'Ground Truth Labels'}</div>
+
+  <div class="swatches">
+    {#if showPredictions}
+      <div class="column">
+        <div class="legend-cell">Correct:</div>
+        <div class="legend-cell">Incorrect:</div>
+      </div>
+    {/if}
+
+    {#each color.domain() as label}
+      <div class="column">
+        <div class="legend-cell">
+          <div class="legend-square" style="background: {color(label)}"></div>
+          <div class="legend-label">{label}</div>
+        </div>
+        {#if showPredictions}
+          <div class="legend-cell">
+            <div class="legend-square"
+              style="background: repeating-linear-gradient(135deg, {color(label)}, {color(label)} 2px, white 2px, white 4px)">
+            </div>
+            <div class="legend-label">{label}</div>
+          </div>
+        {/if}
+      </div>
+    {/each}
+  </div>
+
 </div>
 
 <style>
     #legend {
-      flex: 0 0 20px;
-      max-height: 20px;
-      margin-top: 5px;
+      flex: 0 0 40px;
+      max-height: 40px;
+      margin-left: 5px;
 
       display: flex;
+      align-items: center;
+    }
+
+    .title {
+      margin-right: 1em;
     }
 
     .legend-cell {
@@ -34,19 +54,16 @@
     }
 
     .legend-square {
-      width: 14px;
-      height: 14px;
+      min-width: 14px;
+      min-height: 14px;
       margin-right: 0.5em;
-      margin-left: 1em;
     }
 
-    #incorrect {
-      background: repeating-linear-gradient(
-        135deg,
-        black,
-        black 2px,
-        white 2px,
-        white 4px
-      );
+    .swatches {
+      display: flex;
+    }
+
+    .column {
+      margin-right: 1em;
     }
 </style>
