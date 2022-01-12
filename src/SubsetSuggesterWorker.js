b867be1a4ef3634e855c126c68ace01abe8e6556
Daniel Kerrigan <dkerriga@nd.edu>
2022-01-12 11:10:02 -0500

    best

--- src/SubsetSuggesterWorker.js
+++ src/SubsetSuggesterWorker.js
@@ -1,7 +1,7 @@
 import { getRecommendedSubsets, timeSubsets, topSubsets, randomSubsets, bestTwos, worstTwos } from './SubsetRecommender';
 
 self.onmessage = e => {
-  const recommendedSubsets = bestTwos(e.data);
+  const recommendedSubsets = getRecommendedSubsets(e.data);
   // const recommendedSubsets = randomSubsets(e.data);
   // const recommendedSubsets = timeSubsets(e.data);
   // topSubsets(e.data);
