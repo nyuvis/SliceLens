b1ab1f321f9b7b0c2f5b573d1f81764f2c23a648
Daniel Kerrigan <d.kerrigan111@gmail.com>
2022-03-15 08:36:07 -0400

    hide suggest combos when ratings disabled

--- src/components/controls/FeatureSelector.svelte
+++ src/components/controls/FeatureSelector.svelte
@@ -217,7 +217,9 @@ https://svelte.dev/repl/adf5a97b91164c239cc1e6d0c76c2abe?version=3.14.1
     </select>
   </div>
 
-  <SuggestCombos {criterion} {canAddFeatures} on:set={setFeatureToHighlight}/>
+  {#if criterion.value !== 'none' && ratingsEnabled}
+    <SuggestCombos {criterion} {canAddFeatures} on:set={setFeatureToHighlight}/>
+  {/if}
 {/if}
 
 <div class="label help-row">
